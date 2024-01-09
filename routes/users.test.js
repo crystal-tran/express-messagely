af7f952065d0dec6642c875c14866808039f706e
Crystal Tran <c.t.tran95@gmail.com>
2024-01-09 15:12:31 -0500

    added a beforeEach in users.test

--- routes/users.test.js
+++ routes/users.test.js
@@ -8,3 +8,24 @@ const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
 
 const User = require("../models/user");
 
+let token;
+beforeEach(async function() {
+  let response = await request(app)
+    .post("/auth/register")
+    .send({
+      username: "test1",
+      password: "password",
+      first_name: "Test1",
+      last_name: "Testy1",
+      phone: "+14155550000"
+  });
+  token = response.body.token;
+});
+
+describe("User Routes Test", function () {
+
+
+  test("get list of users", async function(){
+
+  })
+});
