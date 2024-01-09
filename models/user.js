f09e28fc08842364a348b4f0ca91c3e0e681a177
Crystal Tran <c.t.tran95@gmail.com>
2024-01-08 19:31:56 -0500

    fixed things from code review: added NotFoundErrors in some functions

--- models/user.js
+++ models/user.js
@@ -29,7 +29,6 @@ class User {
           ($1, $2, $3, $4, $5, current_timestamp)
        RETURNING username, password, first_name, last_name, phone`,
       [username, hashedPassword, first_name, last_name, phone]);
-    // TODO: could also call last login timestamp (if registering also logging in)
 
     return result.rows[0];
   }
@@ -57,23 +56,24 @@ class User {
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
-    // TODO: check that username exists, throw Not Found error
-    await db.query(
+    const user = await db.query(
       `UPDATE users
           SET last_login_at = current_timestamp
-          WHERE username = $1`,
+          WHERE username = $1
+          RETURNING username`,
       [username]);
+
+    if(user === undefined) throw new NotFoundError();
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
-    // TODO: be explicit with order
     const result = await db.query(
       `SELECT username, first_name, last_name
-      FROM users`
-    );
+       FROM users
+       ORDER BY username`,);
 
     return result.rows;
   }
@@ -88,12 +88,13 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
-    // TODO: throw not found error instead of undefined return
     const result = await db.query(
       `SELECT username, first_name, last_name, phone, join_at, last_login_at
-      FROM users
-      WHERE username = $1`,
+          FROM users
+          WHERE username = $1`,
       [username]);
+
+    if(result === undefined) throw new NotFoundError();
 
     return result.rows[0];
   }
@@ -107,8 +108,14 @@ class User {
    */
 
   static async messagesFrom(username) {
-    // TODO: not found error for if username is not found
-    // TODO: use order by, probably sent_at
+    const user = await db.query(
+      `SELECT username
+          FROM users
+          WHERE username = $1`,
+       [username]);
+
+    if(user === undefined) throw new NotFoundError();
+
     const result = await db.query(
       `SELECT m.id,
               m.to_username,
@@ -120,7 +127,8 @@ class User {
               t.phone AS to_phone
            FROM messages AS m
                 JOIN users AS t ON m.to_username = t.username
-           WHERE m.from_username = $1`,
+           WHERE m.from_username = $1
+           ORDER BY m.sent_at`,
       [username]);
 
     const messages = result.rows.map(m => {
@@ -149,8 +157,14 @@ class User {
    */
 
   static async messagesTo(username) {
-    // TODO: not found error for if username is not found
-    // TODO: use order by, probably sent_at
+    const user = await db.query(
+      `SELECT username
+          FROM users
+          WHERE username = $1`,
+       [username]);
+
+    if(user === undefined) throw new NotFoundError();
+
     const result = await db.query(
       `SELECT m.id,
               m.from_username,
@@ -162,7 +176,8 @@ class User {
               f.phone AS from_phone
           FROM messages AS m
                 JOIN users AS f ON m.from_username = f.username
-          WHERE m.to_username = $1`,
+          WHERE m.to_username = $1
+          ORDER BY m.sent_at`,
       [username]);
 
     const messages = result.rows.map(m => {
