f69e67573384884f920162192274b2728ac6bd30
Crystal Tran <c.t.tran95@gmail.com>
2024-01-08 19:47:55 -0500

    made post route for login and generated token

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,30 @@
 const Router = require("express").Router;
 const router = new Router();
 
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
+
+const User = require("../models/user");
+const Message = require("../models/message");
+const { BadRequestError, UnauthorizedError } = require("../expressError");
+
 /** POST /login: {username, password} => {token} */
+router.post("/login", async function(req, res, next){
+  if(req.body === undefined) throw new BadRequestError();
+
+  const { username, password } = req.body;
+  const result = await User.authenticate(username, password);
+
+  if(result){
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+  throw new UnauthorizedError("Invalid user/password.");
+
+});
+
+
 
 
 /** POST /register: registers, logs in, and returns token.
--- routes/auth.test.js
+++ routes/auth.test.js
@@ -1,27 +1,27 @@
 "use strict";
 
-// const request = require("supertest");
-// const jwt = require("jsonwebtoken");
+const request = require("supertest");
+const jwt = require("jsonwebtoken");
 
-// const app = require("../app");
-// const db = require("../db");
-// const User = require("../models/user");
+const app = require("../app");
+const db = require("../db");
+const User = require("../models/user");
 
 
-// describe("Auth Routes Test", function () {
+describe("Auth Routes Test", function () {
 
-//   beforeEach(async function () {
-//     await db.query("DELETE FROM messages");
-//     await db.query("DELETE FROM users");
+  beforeEach(async function () {
+    await db.query("DELETE FROM messages");
+    await db.query("DELETE FROM users");
 
-//     let u1 = await User.register({
-//       username: "test1",
-//       password: "password",
-//       first_name: "Test1",
-//       last_name: "Testy1",
-//       phone: "+14155550000",
-//     });
-//   });
+    let u1 = await User.register({
+      username: "test1",
+      password: "password",
+      first_name: "Test1",
+      last_name: "Testy1",
+      phone: "+14155550000",
+    });
+  });
 
 //   /** POST /auth/register => token  */
 
@@ -45,37 +45,37 @@
 //     });
 //   });
 
-//   /** POST /auth/login => token  */
+  /** POST /auth/login => token  */
 
-//   describe("POST /auth/login", function () {
-//     test("can login", async function () {
-//       let response = await request(app)
-//         .post("/auth/login")
-//         .send({ username: "test1", password: "password" });
+  describe("POST /auth/login", function () {
+    test("can login", async function () {
+      let response = await request(app)
+        .post("/auth/login")
+        .send({ username: "test1", password: "password" });
 
-//       let token = response.body.token;
-//       expect(jwt.decode(token)).toEqual({
-//         username: "test1",
-//         iat: expect.any(Number)
-//       });
-//     });
+      let token = response.body.token;
+      expect(jwt.decode(token)).toEqual({
+        username: "test1",
+        iat: expect.any(Number)
+      });
+    });
 
-//     test("won't login w/wrong password", async function () {
-//       let response = await request(app)
-//         .post("/auth/login")
-//         .send({ username: "test1", password: "WRONG" });
-//       expect(response.statusCode).toEqual(401);
-//     });
+    test("won't login w/wrong password", async function () {
+      let response = await request(app)
+        .post("/auth/login")
+        .send({ username: "test1", password: "WRONG" });
+      expect(response.statusCode).toEqual(401);
+    });
 
-//     test("won't login w/wrong password", async function () {
-//       let response = await request(app)
-//         .post("/auth/login")
-//         .send({ username: "not-user", password: "password" });
-//       expect(response.statusCode).toEqual(401);
-//     });
-//   });
-// });
+    test("won't login w/wrong password", async function () {
+      let response = await request(app)
+        .post("/auth/login")
+        .send({ username: "not-user", password: "password" });
+      expect(response.statusCode).toEqual(401);
+    });
+  });
+});
 
-// afterAll(async function () {
-//   await db.end();
-// });
+afterAll(async function () {
+  await db.end();
+});
