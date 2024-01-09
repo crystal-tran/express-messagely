f6ec511894f91eb298ec5f951a3bf5658f62990b
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-08 16:58:54 -0800

    added register route

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ const Message = require("../models/message");
 const { BadRequestError, UnauthorizedError } = require("../expressError");
 
 /** POST /login: {username, password} => {token} */
-router.post("/login", async function(req, res, next){
+router.post("/login", async function(req, res){
   if(req.body === undefined) throw new BadRequestError();
 
   const { username, password } = req.body;
@@ -25,12 +25,25 @@ router.post("/login", async function(req, res, next){
   throw new UnauthorizedError("Invalid user/password.");
 });
 
-
-
-
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post("/register", async function(req, res) {
+  if(req.body === undefined) throw new BadRequestError();
+
+  const { username, password, first_name, last_name, phone } = req.body;
+
+  const result = await User.register(
+    { username, password, first_name, last_name, phone }
+  );
+
+  if (result !== undefined) {
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+  throw new BadRequestError("User registration failed.");
+})
 
 module.exports = router;
